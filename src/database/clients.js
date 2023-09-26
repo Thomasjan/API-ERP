479ef91c7eafb1838752a2dd10a65ae085f6e851
Emmanuel Triballier <emmanuel.triballier@gestimum.com>
2023-09-26 09:26:41 +0000

    Suppression de lignes vides dans clients.js

--- src/database/clients.js
+++ src/database/clients.js
@@ -74,8 +74,6 @@ class Clients {
         }
     }
 
-
-
     async getGestimumClients() {
         const pool = await mssql.connect(config)
         const sql = `SELECT PCF_CODE, PCF_RS, PCF_EMAIL, PCF_RUE, PCF_CP, PCF_VILLE, PAY_CODE, PCF_TYPE  
@@ -87,8 +85,6 @@ class Clients {
             clients: res.recordset
         }
     }
-
-    
 }
 
 module.exports = new Clients()
