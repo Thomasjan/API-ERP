d9f041941252a6f5f109b16270e048279e62198f
Thomas Jankowski <thomas.jankowski@gestimum.com>
2023-11-29 07:54:32 +0100

    ACtions.getAll() with query params Display -> return fields to display

--- src/database/actions.js
+++ src/database/actions.js
@@ -8,25 +8,52 @@ class Actions {
     async getAll(req, res) {
         console.log(`Actions.getAll()`.yellow);
         try {
-            // let sql = `SELECT ${DISPLAY} FROM ACTIONS WHERE ${FILTER} ORDER BY AFF_CODE`;
-            //40 first actions
+            // Default SQL query for all fields
             let sql = `SELECT * FROM ACTIONS ORDER BY ACT_DTMAJ DESC OFFSET 0 ROWS FETCH NEXT 40 ROWS ONLY`;
-    
     
             // Check if there are query parameters
             const queryParams = req.query;
-
+    
             if (Object.keys(queryParams).length > 0) {
                 console.log('Query parameters found: ', queryParams);
-                // Construct the WHERE clause based on query parameters
+    
+                // Check for the 'display' parameter
+                if (queryParams.display) {
+                    try {
+                        // Remove single quotes and parse the display parameter into an array
+                        const displayFields = JSON.parse(queryParams.display);
+                
+                        // Check if displayFields is an array
+                        if (Array.isArray(displayFields)) {
+                
+                            // Construct the SELECT clause with the specified fields
+                            const selectClause = displayFields.join(', ');
+                
+                            // Update the SQL query
+                            sql = `SELECT ${selectClause} FROM ACTIONS ORDER BY ACT_DTMAJ DESC OFFSET 0 ROWS FETCH NEXT 40 ROWS ONLY`;
+                        } else {
+                            console.error('Invalid display parameter format. Expected an array.');
+                            res.status(400).json({ Error: 'Invalid display parameter format. Expected an array.' });
+                            return;
+                        }
+                    } catch (parseError) {
+                        console.error('Error parsing display parameter:', parseError);
+                        res.status(400).json({ Error: 'Invalid display parameter format.' });
+                        return;
+                    }
+                }
+    
+                // Construct the WHERE clause based on other query parameters if needed
                 const whereConditions = Object.entries(queryParams)
+                    .filter(([key]) => key !== 'display') // Exclude 'display' from where conditions
                     .map(([key, value]) => `${key}='${value}'`)
                     .join(' AND ');
     
-                sql = `SELECT * FROM ACTIONS WHERE ${whereConditions} ORDER BY ACT_DTMAJ DESC OFFSET 0 ROWS FETCH NEXT 40 ROWS ONLY`;
-                // sql = `SELECT ${DISPLAY} FROM AFFAIRES WHERE ${FILTER} AND ${whereConditions} ORDER BY AFF_CODE`;
+                if (whereConditions) {
+                    sql = `SELECT * FROM ACTIONS WHERE ${whereConditions} ORDER BY ACT_DTMAJ DESC OFFSET 0 ROWS FETCH NEXT 40 ROWS ONLY`;
+                }
             }
-            
+    
             const pool = await mssql.connect(config);
             const result = await pool.request().query(sql);
     
@@ -39,7 +66,6 @@ class Actions {
             res.status(500).json({ Erreur: error.toString() });
         }
     }
-
     
 }
 
